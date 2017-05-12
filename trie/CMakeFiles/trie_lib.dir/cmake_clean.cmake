f678d8d64c1aac1353e58d2ede684212839f640a
Jon <jgodbou@gmail.com>
2017-05-12 19:41:43 -0400

    New things, this is untidy, sorry

---
+++ trie/CMakeFiles/trie_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trie_lib.dir/trie.cc.o"
+  "libtrie_lib.pdb"
+  "libtrie_lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trie_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
